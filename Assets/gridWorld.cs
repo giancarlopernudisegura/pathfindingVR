63c1662fa1ab0ceda195e1ba08bc8079404e3e11
apklps <apklpsltd@gmail.com>
2020-02-04 15:56:52 -0700

    created gridWorld template
    Co-Authored-By: Giancarlo Pernudi Segura <giancarlopernudisegura@users.noreply.github.com>

---
+++ Assets/gridWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridWorld : MonoBehaviour
+{
+    public int size;
+    public List<Vector2> walls;
+    public Vector2 start;
+    public Vector2 goal;
+
+    public gridWorld(int size, List<Vector2> walls, Vector2 start, Vector2 goal)
+    {
+        this.size = size;
+        this.walls = walls;
+        this.start = start;
+        this.goal = goal;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
