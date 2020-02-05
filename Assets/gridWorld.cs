1262a6b3f37f59e1ad9379218120f02eb85c6bb4
giancarlopernudisegura <pernudig@gmail.com>
2020-02-04 22:07:04 -0700

    actually creates a plane when run

--- Assets/gridWorld.cs
+++ Assets/gridWorld.cs
@@ -6,21 +6,22 @@ public class gridWorld : MonoBehaviour
 {
     public int size;
     public List<Vector2> walls;
-    public Vector2 start;
-    public Vector2 goal;
-
-    public gridWorld(int size, List<Vector2> walls, Vector2 start, Vector2 goal)
-    {
-        this.size = size;
-        this.walls = walls;
-        this.start = start;
-        this.goal = goal;
-    }
+    public Vector3 start;
+    public Vector3 goal;
+    public int gridUnit = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        size = 8;
+        walls = new List<Vector2>();
+        start = new Vector3(0, 0, 0);
+        goal = new Vector3(7, 0, 7);
+
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane.transform.position = Vector3.zero;
+        int planeSize = gridUnit * this.size;
+        plane.transform.localScale = new Vector3(planeSize, 1, planeSize);
     }
 
     // Update is called once per frame
