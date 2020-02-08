12fc030f34275aa2dfeed365fcd5fa8bb376c3fd
giancarlopernudisegura <pernudig@gmail.com>
2020-02-08 15:19:13 -0700

    creating plane prefab

--- Assets/gridWorld.cs
+++ Assets/gridWorld.cs
@@ -9,6 +9,7 @@ public class gridWorld : MonoBehaviour
     public Vector3 start;
     public Vector3 goal;
     public int gridUnit = 1;
+    public GameObject plane;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,7 @@ public class gridWorld : MonoBehaviour
         start = new Vector3(0, 0, 0);
         goal = new Vector3(7, 0, 7);
 
-        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        plane.transform.position = Vector3.zero;
+        Instantiate(plane, Vector3.zero, Quaternion.identity);
         int planeSize = gridUnit * this.size;
         plane.transform.localScale = new Vector3(planeSize, 1, planeSize);
     }
