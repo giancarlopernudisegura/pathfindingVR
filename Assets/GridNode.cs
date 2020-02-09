ad76bc4a68ffb92c27be81392e84d4ae2f625f7d
giancarlopernudisegura <pernudig@gmail.com>
2020-02-09 16:57:05 -0700

    added grid nodes

---
+++ Assets/GridNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.MonoBehaviour;
+
+public class GridNode
+{
+    public bool Visible = true;
+    public Vector3 Location;
+    public List<GridNode> Neighbors = new List<GridNode>();
+    public GridNode Parent = null;
+    public ushort GScore = 0;
+
+    // constructor
+    public GridNode(GridNode _parent, Vector3 loc)
+    {
+        this.Parent = _parent;
+        this.Location = loc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/gridWorld.cs
+++ Assets/gridWorld.cs
@@ -2,55 +2,130 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gridWorld : MonoBehaviour
+public class GridWorld : MonoBehaviour
 {
-    public float size;
-    public List<Vector3> walls;
-    public Vector3 start;
-    public Vector3 goal;
-    public float gridUnit = 1;
-    public GameObject plane;
-    public GameObject wallObj;
-    public GameObject goalObj;
+    public static float Size = 8;
+    public List<Vector3> Walls = new List<Vector3>();
+    public GridNode StartNode;
+    public Vector3 StartLoc;
+    public Vector3 Goal;
+    public static float GridUnit = 1;
+    public GameObject Plane;
+    public GameObject WallObj;
+    public GameObject GoalObj;
+    public GameObject Node;
 
     // Start is called before the first frame update
     void Start()
     {
-        size = 8;
-        walls = new List<Vector3>();
-        for (int i = 0; i < size; i += 2)
-        {
-            walls.Add(new Vector3(i, 1, i));
-        }
-        start = new Vector3(0, 0, 0);
-        goal = new Vector3(3, 0, 7);
-
-        float planeSize = gridUnit * this.size;
-        plane.transform.localScale = new Vector3(planeSize, 1, planeSize);
-        Instantiate(plane, Vector3.zero, Quaternion.identity);
-
-        foreach (Vector3 wallVtr in walls)
-        {
-            Vector3 worldVector = ConvertToWorld(wallVtr);
-            wallObj.transform.localScale = new Vector3(gridUnit, 1, gridUnit);
-            Instantiate(wallObj, worldVector, Quaternion.identity);
-        }
-
-        Instantiate(goalObj, ConvertToWorld(goal), Quaternion.identity);
+        SpawnPlane();
+        SpawnWalls();
+        SpawnGoal();
+        SpawnGrid();
+        DisplayNodes();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    private Vector3 ConvertToWorld(Vector3 input)
+    private void SpawnPlane()
+    {
+        float planeSize = GridUnit * Size;
+        Plane.transform.localScale = new Vector3(planeSize, 1, planeSize);
+        Instantiate(Plane, Vector3.zero, Quaternion.identity);
+    }
+
+    private void SpawnWalls()
+    {
+        foreach (Vector3 wallVtr in Walls)
+        {
+            Vector3 worldVector = ConvertToWorld(wallVtr);
+            WallObj.transform.localScale = new Vector3(GridUnit, 1, GridUnit);
+            Instantiate(WallObj, worldVector, Quaternion.identity);
+        }
+    }
+
+    private void SpawnGoal()
+    {
+        Instantiate(GoalObj, ConvertToWorld(Goal), Quaternion.identity);
+    }
+
+    private void SpawnGrid()
+    {
+        Dictionary<Vector3, GridNode> nodeDictionary = new Dictionary<Vector3, GridNode>();
+
+        // create grid node
+        for (byte i = 0; i < Size; i++)
+        {
+            for (byte j = 0; j < Size; j++)
+            {
+                Vector3 v = new Vector3(i, 1, j);
+                if (!IsObstacle(v))
+                {
+                    GridNode g = new GridNode(null, v);
+                    nodeDictionary[v] = g;
+                }
+            }
+        }
+
+        // add neighbors
+        foreach (Vector3 v in nodeDictionary.Keys)
+        {
+            // add up
+            if (nodeDictionary.ContainsKey(v + Vector3.forward))
+            {
+                nodeDictionary[v].Neighbors.Add(nodeDictionary[v + Vector3.forward]);
+            }
+            // add down
+            if (nodeDictionary.ContainsKey(v + Vector3.back))
+            {
+                nodeDictionary[v].Neighbors.Add(nodeDictionary[v + Vector3.back]);
+            }
+            // add right
+            if (nodeDictionary.ContainsKey(v + Vector3.right))
+            {
+                nodeDictionary[v].Neighbors.Add(nodeDictionary[v + Vector3.right]);
+            }
+            // add left
+            if (nodeDictionary.ContainsKey(v + Vector3.left))
+            {
+                nodeDictionary[v].Neighbors.Add(nodeDictionary[v + Vector3.left]);
+            }
+        }
+
+        StartNode = nodeDictionary[StartLoc];
+    }
+
+    private void DisplayNodes()
+    {
+        foreach (GridNode n in StartNode.Neighbors)
+        {
+            Vector3 v = ConvertToWorld(n.Location);
+            v.y -= .5f - (Node.transform.localScale.y / 2);
+            Instantiate(Node, v, Quaternion.identity);
+        }
+    }
+
+    public static Vector3 ConvertToWorld(Vector3 input)
     {
         Vector3 output = Vector3.zero;
-        output.x = (input.x * gridUnit) - (gridUnit * this.size / 2) + (gridUnit / 2);
-        output.z = (input.z * gridUnit) - (gridUnit * this.size / 2) + (gridUnit / 2);
+        output.x = (input.x * GridWorld.GridUnit) - (GridWorld.GridUnit * GridWorld.Size / 2) + (GridWorld.GridUnit / 2);
+        output.z = (input.z * GridWorld.GridUnit) - (GridWorld.GridUnit * GridWorld.Size / 2) + (GridWorld.GridUnit / 2);
         output.y = 1;
         return output;
+    }
+
+    public bool IsObstacle(Vector3 v)
+    {
+        foreach (Vector3 wallV in Walls)
+        {
+            if (wallV.x == v.x && wallV.z == v.z)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
